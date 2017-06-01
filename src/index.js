d7299c2b49543867a72c36b7bec29cd7a5d360e0
litvinenkoEvgeny <youtbisnesspartner@gmail.com>
2017-06-01 17:42:20 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+const App=function () {
+    console.log('App:init');
+
+};
+const app= new App();
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    watch: !isProduction,
+    devtool: !isProduction && 'cheap-inline-module-source-map',
+    devServer: {
+        port: 9000,
+        historyApiFallback: true,
+        contentBase: 'src/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+             {
+                test:   /\.css$/,
+                loader: 'style!css'
+            },
+            {
+                test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                loader: 'file?name=[path][name].[ext]'
+            }
+        ]
+    },
+    plugins: [],
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions: ['', '.js']
+    },
+    resolveLoader: {
+        modulesDirectories: ['node_modules'],
+        moduleTemplates: ['*-loader', '*'],
+        extensions: ['', '.js']
+    },
+};
+
+if (isProduction) {
+    module.exports.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {'NODE_ENV': JSON.stringify('production')}
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {warnings: false},
+        })
+    );
+}
